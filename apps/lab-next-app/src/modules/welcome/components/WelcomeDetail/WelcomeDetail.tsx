75f76591255988f8e51595eab54d757099fb57d5
Anton Skarabahaty <anton.skarabahaty@itechart-group.com>
2023-06-12 09:33:18 +0000

    Refactor/table logic

--- apps/lab-next-app/src/modules/welcome/components/WelcomeDetail/WelcomeDetail.tsx
+++ apps/lab-next-app/src/modules/welcome/components/WelcomeDetail/WelcomeDetail.tsx
@@ -1,68 +1,14 @@
 import { Typography } from '@mui/material'
-import { GridColDef, GridPaginationModel, GridRowsProp } from '@mui/x-data-grid'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
 
-import Table from '@/src/components/Table'
-import { useCoursesQuery } from '@/src/mock-data/grahpql-hooks'
-
-const columns: GridColDef[] = [
-  {
-    field: 'name',
-    headerName: 'Name',
-    flex: 1,
-    renderCell: (params) => (
-      <Link
-        href={{
-          pathname: '/my-labs/[id]',
-          query: { id: params.row.id },
-        }}
-      >
-        {params.value}
-      </Link>
-    ),
-  },
-  { field: 'description', headerName: 'Description', flex: 1 },
-]
-
-const initialPagination = {
-  page: 0,
-  pageSize: 5,
-}
+import CoursesTable from '@/src/components/Tables/CoursesTable'
 
 function WelcomeDetail() {
-  const [paginationModel, setPaginationModel] = useState(initialPagination)
-
-  const { data, loading, refetch, total } = useCoursesQuery(initialPagination)
-
-  const router = useRouter()
-
-  const handleAddNewCourse = () => {
-    router.push({
-      pathname: '/new-lab',
-    })
-  }
-
-  const handlePagination = (model: GridPaginationModel) => {
-    refetch(model)
-    setPaginationModel(model)
-  }
-
   return (
     <>
       <Typography fontSize={30} p={3}>
         Course Management
       </Typography>
-      <Table
-        columns={columns}
-        loading={loading}
-        rows={data?.courses ? (data.courses as GridRowsProp) : []}
-        onClick={handleAddNewCourse}
-        onPagination={handlePagination}
-        rowsCount={total}
-        paginationModel={paginationModel}
-      />
+      <CoursesTable />
     </>
   )
 }
